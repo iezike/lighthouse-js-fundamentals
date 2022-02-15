d19cb95e9afb6cc9eac2342c6f0ef867d950f2a6
Ikenna Johnking Ezike <ezike_iyke@yahoo.com>
2022-02-15 04:10:55 +0000

    Add more conditionals examples

--- conditionals.js
+++ conditionals.js
@@ -14,9 +14,21 @@ if (cold) {
 console.log("Now you're ready to go outside!");
 */
 
-// Example 2
 
-const temperature =  40;
+// Example 2 
+/*const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+*/
+
+
+// Example 3
+
+/*const temperature =  40;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -27,3 +39,26 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+*/
+
+// Example 4 
+/*const isCitizen = true;
+const age = 17;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+*/
+
+//Example 5 
+/*const temperature =  41;
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+  }*/
+
+// Example 6
+const raining = true
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
